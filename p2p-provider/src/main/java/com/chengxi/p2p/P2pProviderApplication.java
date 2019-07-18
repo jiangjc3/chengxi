1624e882a555095d47f4c4abcc86dfa32b89cf94
yayaaichirourou <jiangjc3@foxmail.com>
2019-07-18 22:55:13 +0800

    init

---
+++ p2p-provider/src/main/java/com/chengxi/p2p/P2pProviderApplication.java
@@ -0,0 +1,13 @@
+package com.chengxi.p2p;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P2pProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(P2pProviderApplication.class, args);
+    }
+
+}
